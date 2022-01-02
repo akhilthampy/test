54d4cbdebacd29865feebe91da404e5deb466319
Akhil Thampy <68418451+akhilwrist@users.noreply.github.com>
2022-01-02 21:05:55 +0530

    Update emailpass.py

--- emailpass.py
+++ emailpass.py
@@ -1,3 +1,11 @@
+print(r""" ______                          _         _ _       
+(____  \                        | |       | (_)      
+ ____)  ) ____ _____ _____  ____| |__   __| |_  ____ 
+|  __  ( / ___) ___ (____ |/ ___)  _ \ / _  | |/ ___)
+| |__)  ) |   | ____/ ___ ( (___| | | ( (_| | | |    
+|______/|_|   |_____)_____|\____)_| |_|\____|_|_|    
+                                                     
+             https://github.com/akhilwrist                                               """)
 import requests
 c=input("enter email:- ")
 url = "https://breachdirectory.p.rapidapi.com/"
